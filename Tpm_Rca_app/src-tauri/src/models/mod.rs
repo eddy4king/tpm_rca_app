004102702fd3d2ac42b577956ebcb047a8af3f07
Edosa Ilawe Joseph <eddyking0@gmail.com>
2026-05-06 09:24:06 +0100

    feat: add Equipment model

--- Tpm_Rca_app/src-tauri/src/models/mod.rs
+++ Tpm_Rca_app/src-tauri/src/models/mod.rs
@@ -1,1 +1,20 @@
+use serde::{Deserialize, Serialize};
+use sqlx::FromRow;
 
+
+
+#[derive(Debug, Serialize, Deserialize, FromRow, Clone)]
+
+pub struct Equipment {
+    pub id : String,
+    pub tag_number : String,
+    pub name : String,
+    pub description : Option<String>,
+    pub location : String,
+    pub criticality : String,
+    pub status: String,
+    pub equipment_type : String,
+    pub parent_id : Option<String>,
+    pub created_at : String,
+    pub updated_at : String
+}
